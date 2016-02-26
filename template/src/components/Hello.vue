0fad7cea002196f881b0c1ada838ab0ccaa0c0da
elclanrs <elclanrs@gmail.com>
2016-02-26 16:15:36 -0500

    first commit

---
+++ template/src/components/Hello.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="hello">
+    <h1>{{msg}}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      msg: 'Hello World!'
+    }
+  }
+}
+</script>
+
+<style lang="stylus">
+
+</style>
